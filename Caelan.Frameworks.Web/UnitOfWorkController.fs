8f3a7d48bc7b35a0439aff0036d7b957dca3f150
Matteo Bruni <ar3shack@gmail.com>
2014-11-09 13:08:42 +0100

    UnitOfWorkCaller implementation by delegation

--- Caelan.Frameworks.Web/UnitOfWorkController.fs
+++ Caelan.Frameworks.Web/UnitOfWorkController.fs
@@ -10,22 +10,22 @@ open Caelan.Frameworks.BIZ.Classes
 [<AbstractClass>]
 type GenericUnitOfWorkController<'TUnitOfWork when 'TUnitOfWork :> IUnitOfWork and 'TUnitOfWork :> IDisposable and 'TUnitOfWork : (new : unit -> 'TUnitOfWork)>() = 
     inherit Controller()
-    member __.UnitOfWork<'T>(call: Func<IUnitOfWork, 'T>) = using (new 'TUnitOfWork()) (fun manager -> call.Invoke(manager))
-    member __.UnitOfWork(call: Action<IUnitOfWork>) = using (new 'TUnitOfWork()) (fun manager -> call.Invoke(manager))
+
+    let uowCaller = GenericUnitOfWorkCaller<'TUnitOfWork>()
+    member __.UnitOfWork<'T>(call: Func<IUnitOfWork, 'T>) = uowCaller.UnitOfWork<'T>(call)
+    member __.UnitOfWork(call: Action<IUnitOfWork>) = uowCaller.UnitOfWork(call)
 
     member this.Repository<'T, 'TRepository when 'TRepository :> IRepository>(call: Func<'TRepository, 'T>) =
-        this.UnitOfWork(fun t -> call.Invoke(t.Repository<'TRepository>()))
+        uowCaller.Repository<'T, 'TRepository>(call)
 
     member this.Repository<'T, 'TEntity, 'TDTO when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null and 'TDTO : not struct and 'TDTO : equality and 'TDTO : null>(call: Func<IRepository<'TEntity, 'TDTO>, 'T>) =
-        this.UnitOfWork(fun t -> call.Invoke(t.Repository<'TEntity, 'TDTO>()))
+        uowCaller.Repository<'T, 'TEntity, 'TDTO>(call)
 
     member this.RepositoryList<'TEntity, 'TDTO when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null and 'TDTO : not struct and 'TDTO : equality and 'TDTO : null>() =
-        this.Repository<seq<'TDTO>, 'TEntity, 'TDTO>(fun t -> t.List())
+        uowCaller.RepositoryList<'TEntity, 'TDTO>()
 
     member this.UnitOfWorkCallSaveChanges(call: Action<IUnitOfWork>) =
-        this.UnitOfWork(fun t ->
-            call.Invoke(t)
-            t.SaveChanges() <> 0)
+        uowCaller.UnitOfWorkCallSaveChanges(call)
 
     member __.Empty() = EmptyResult()
 
